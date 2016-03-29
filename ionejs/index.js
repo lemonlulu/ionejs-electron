0502b6708be644b5cdade6e2901f957bb432d4da
phyxdown <1768649634@qq.com>
2016-03-29 06:02:23 -0400

    trying to construct ide for ionejs

---
+++ ionejs/index.js
@@ -0,0 +1,53 @@
+var ionejs = require("ionejs");
+var descriptors = require("./descriptors");
+var ones = require("./ones")
+
+var Creator = function(){
+	this._ones = {};
+	this._descriptors = {};
+};
+
+var p = Creator.prototype;
+
+p.setOne = function(alias, constructor){
+	this._ones[alias] = constructor;
+};
+
+p.setOnes = function(constructors) {
+	this._ones = constructors;
+};
+
+p.setDescriptor = function(alias, constructor){
+	this._descriptors[alias] = constructor;
+}
+
+p.setDescriptors = function(constructors) {
+	this._descriptors = constructors;
+};
+
+p.parse = function(config){
+	var me = this;
+	var _parse = function(config){
+		var options = config.options;
+		var children = config.children;
+
+		var G = me._ones[config.alias]
+		var D = me._descriptors[config.alias];
+
+		var one = new G(options);
+		var descriptor = new D(options);
+
+		descriptor.addChild(one);
+		
+		for(var i = 0, l = children.length; i < l; i++){
+			var child = _parse(children[i]);
+			one.addChild(child);
+		}
+		return descriptor;
+	}
+
+	return _parse(config);
+
+};
+
+module.exports = Creator;
