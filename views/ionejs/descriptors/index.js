30b946b3b9d8a50dbb692020f0191c5b8a4c590a
phyxdown <1768649634@qq.com>
2016-04-10 01:07:13 +0800

    describe SpinWriter

--- views/ionejs/descriptors/index.js
+++ views/ionejs/descriptors/index.js
@@ -3,3 +3,4 @@ module.exports['painters.Animator'] = require("./painter");
 module.exports['painters.RotatingPainter'] = require("./painter");
 module.exports['Stage'] = require('./stage');
 module.exports['Writer'] = require('./writer');
+module.exports['writers.SpinWriter'] = require('./writer');
