f7ce16328b3e8709129a24dbd239b93dbb95dbca
phyxdown <1768649634@qq.com>
2016-04-27 05:35:06 -0400

    use ionejs 1.2

--- main.js
+++ main.js
@@ -1,60 +1,36 @@
 var ionejs = require("ionejs");
 var uonejs = require("./ionejs");
 
-var stageConfig = {
-    "alias": "Stage",
+var writerConfig = {
+    "alias": "Writer",
     "options": {
+        "text": "Hackation",
+        "color": "#7BCBEB",
+        "shouldMeasure": false,
         "hitable": true,
-        "width": 1667,
-        "height": 611,
+        "width": 112.24609375,
+        "height": 20,
+        "prefix": "",
+        "align": "start",
+        "baseline": "top",
         "active": true,
         "visible": true,
-        "moveable": false,
+        "moveable": true,
         "dropable": false,
-        "x": 0,
-        "y": 0,
+        "x": 147,
+        "y": 327,
         "regX": 0,
         "regY": 0,
         "rotation": 0,
-        "scaleX": 1,
-        "scaleY": 1,
+        "scaleX": 2,
+        "scaleY": 2,
         "skewX": 0,
         "skewY": 0,
         "alpha": 1
     },
-    "children": [{
-            "alias": "Writer",
-            "options": {
-                "text": "Hackation",
-                "color": "#7BCBEB",
-                "shouldMeasure": false,
-                "hitable": true,
-                "width": 112.24609375,
-                "height": 20,
-                "prefix": "",
-                "align": "start",
-                "baseline": "top",
-                "active": true,
-                "visible": true,
-                "moveable": true,
-                "dropable": false,
-                "x": 147,
-                "y": 327,
-                "regX": 0,
-                "regY": 0,
-                "rotation": 0,
-                "scaleX": 2,
-                "scaleY": 2,
-                "skewX": 0,
-                "skewY": 0,
-                "alpha": 1
-            },
-            "children": []
-        }]
+    "children": []
 };
 
-var stage = uonejs.create(stageConfig);
-ionejs.instance.init(stage, document.getElementById('app'));
-ionejs.instance.run();
-ionejs.instance.dropable();
-ionejs.instance.moveable();
+var stage = new ionejs.Stage('app');
+var writer = ionejs.create(writerConfig);
+stage.addChild(writer);
